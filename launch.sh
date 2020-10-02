8a705f0290fc43d360be773bdeb62eb6cfc24b85
Dx55 <45577743+Dx55@users.noreply.github.com>
2020-10-02 13:52:39 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 BASEDIR='/var/mobile'
 IP='192.168.1.4'
-#idke I don't use android or some thin
-#SCREENSHOT_LOCATION='/storage/emulated/0/Pictures/Screenshots'
-SCREENSHOT_LOCATION='/var/mobile/Media/DCIM/100APPLE'
+# Itoddler btfo
+SCREENSHOT_LOCATION='/storage/emulated/0/Pictures/Screenshots'
 
 while true;
 do
