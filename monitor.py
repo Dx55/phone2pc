e45c8c2e07e3370890e7c7e927c455fdda298295
Dx55 <45577743+Dx55@users.noreply.github.com>
2020-10-02 14:04:38 +0200

    no sleep
    i cant sleep, why the script can sleep?

--- monitor.py
+++ monitor.py
@@ -1,9 +1,8 @@
-import os, time
+import os
 path_to_watch = "Media/DCIM/100APPLE"  #idke if androed is diffrent
 before = dict ([(f, None) for f in os.listdir (path_to_watch)])
 #print(before)
 while 1:
-  time.sleep(2)
   after = dict ([(f, None) for f in os.listdir (path_to_watch)])
   added = [f for f in after if not f in before]
   #removed = [f for f in before if not f in after]
